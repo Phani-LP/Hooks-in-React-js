60d4d7aa7157e4e005a623ba695b1f8df68dfb55
D. Naga Phanindra <104689319+Phani-LP@users.noreply.github.com>
2023-12-30 13:42:32 +0530

    Example

--- useRef Hook.jsx
+++ useRef Hook.jsx
@@ -1,3 +1,9 @@
+//The useRef hook is a great way to store a mutable value that doesn't 
+//trigger a re-render. The useRef hook returns an object with a current 
+//property, which is mutable and can hold any value. const inputRef = useRef(); 
+//With this we've created a ref that we can assign to an input field.
+
+
 import { useState } from "react";
 import { useRef } from "react";
 import "./App.css";
